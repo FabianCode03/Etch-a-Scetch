a652003bee5b5a23461e7ae0d4d43d132130c15d
FabianCode03 <behrendtf5@gmail.com>
2023-09-15 07:02:31 +0200

    Add styled borders, auto-calculate pixelWidth

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 const pixelContainer = document.querySelector(".pixel-container");
 
-// const numberOfPixels = 16;
-
 console.log(pixelContainer);
 
-function addPixels(numberOfPixels) {
-  for (let i = 0; i < numberOfPixels; i++) {
+function addPixels(pixelsPerRow) {
+  for (let i = 0; i < pixelsPerRow * pixelsPerRow; i++) {
     const tempPixel = document.createElement("div");
     tempPixel.classList.add("pixel");
+    tempPixel.style.flexBasis = `${100 / pixelsPerRow}%`;
     pixelContainer.appendChild(tempPixel);
   }
 }
 
-addPixels(100);
+addPixels(10);
